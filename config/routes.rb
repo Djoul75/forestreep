70f6896d92e4495ab1a915017e3e7d10a17c25d0
thomas compeyron <compeyron.thomas@live.com>
2022-02-22 08:56:26 +0100

    updated router

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users, only: %i[new create] do
-    resources :forests, only: %i[new create edit update delete]
+  resources :users, only: %i[new create edit update delete] do
+    resources :forests, only: %i[new create edit update]
   end
-  resources :forests, only: %i[index show] do
+  resources :forests, only: %i[index show delete] do
     resources :reviews, only: %i[new create]
   end
 end
