fa47406f2f7ba42f5a4d0098f5eeed187afc45af
Kevin Leblond <leblond_kevin@hotmail.fr>
2022-02-22 12:45:27 +0100

    Dependent destroy à jour pour Forest Review + Booking

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :forests
-  has_many :bookings
-  has_many :reviews
+  has_many :bookings, dependent: :destroy
+  # has_many :reviews #Commented car pas de user_review en place pour l'instant
 
   # validates :first_name, :last_name, presence: true
   # validates :first_name, :last_name, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
